73e5190e289bdf6f99a630a33db7b96e2cebf6b4
Rifqiii <rifqibkl14@gmail.com>
2023-02-05 21:50:14 +0700

    asd

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -42,10 +42,10 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      repo: 'MdRifqii/WebsEdit',
+      repositoryId: 'R_kgDOI4wUlg',
+      category: 'General',
+      categoryId: 'DIC_kwDOI4wUls4CUAwa',
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
